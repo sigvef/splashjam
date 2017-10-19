1790e09a60d690b4437b55cc52cab3bd130e2812
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2017-10-19 12:26:58 +0200

    Add physics engine

--- GameState.js
+++ GameState.js
@@ -6,14 +6,19 @@ GameState.prototype.init = function() {
   this.scene = new THREE.Scene();
   this.camera = new THREE.Camera();
   this.camera = new THREE.PerspectiveCamera(25, 16 / 9, 1, 1000); 
-  this.camera.position.z = 20;
-  this.stick = new THREE.Mesh(
-      new THREE.BoxGeometry(2, 0.1, 0.1),
+  this.camera.position.z = 200;
+  this.ball = new THREE.Mesh(
+      new THREE.SphereGeometry(2, 0.1, 0.1),
       new THREE.MeshBasicMaterial({color: 0xff0000}));
-  this.stick.position.x = 1;
-  this.stickGroup = new THREE.Object3D();
-  this.stickGroup.add(this.stick);
-  this.scene.add(this.stickGroup);
+  this.scene.add(this.ball);
+
+  this.matterEngine = Matter.Engine.create();
+  this.physicsBall = Matter.Bodies.circle(0, 0, 1, { density: 0.04, frictionAir: 0.005});
+  Matter.World.add(this.matterEngine.world, this.physicsBall);
+  Matter.World.add(this.matterEngine.world, Matter.Constraint.create({
+            pointA: { x: 5, y: 5 },
+            bodyB: this.physicsBall
+                  }));
 
   this.rotDz = 0;
 };
@@ -22,10 +27,12 @@ GameState.prototype.pause = function() {
 };
 
 GameState.prototype.resume = function() {
-
 };
 
 GameState.prototype.render = function(renderer) {
+  this.ball.position.x = this.physicsBall.position.x;
+  this.ball.position.y = -this.physicsBall.position.y;
+  this.ball.rotation.z = this.physicsBall.angle;
   renderer.render(this.scene, this.camera);
 };
 
@@ -41,5 +48,7 @@ GameState.prototype.update = function() {
   if(this.rotDz > 2) {
     this.rotDz = 2;
   }
-  this.stickGroup.rotation.z += this.rotDz;
+  this.ball.rotation.z += this.rotDz;
+
+  Matter.Engine.update(this.matterEngine);
 };
