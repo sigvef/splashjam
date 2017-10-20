42779ab9aa7b7a85bc88b56ddc6742118c39d029
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2017-10-20 12:21:47 +0200

    Add score display circles

--- HUD.js
+++ HUD.js
@@ -32,6 +32,34 @@ HUD.prototype.render = function() {
   this.ctx.textAlign = 'right';
   this.ctx.fillText(parseInt(this.game.scores[1]).toString(), 1920 - 40, 20);
 
+  this.ctx.strokeStyle = 'rgba(255, 255, 255, .2)';
+  this.ctx.lineWidth = 4;
+  for(let i = 0; i < 10; i++) {
+    this.ctx.beginPath();
+    const x = 1920 / 2 + (i-4.5) * 100;
+    const y = 40;
+    this.ctx.arc(x, y, 30, 0, Math.PI * 2);
+    if(i < 5) {
+      const color = new THREE.Color(this.game.player1.options.color);
+      const style = `rgba(${color.r * 255 | 0}, ${color.g * 255 | 0}, ${color.b * 255 | 0}, 0.5`;
+      this.ctx.strokeStyle = style;
+      this.ctx.stroke();
+      if(4 - i - this.game.scores[0] < 0) {
+        this.ctx.fillStyle = `rgba(${2 * color.r * 255 | 0}, ${2 * color.g * 255 | 0}, ${2 * color.b * 255 | 0}, 0.8)`;
+        this.ctx.fill();
+      }
+    } else {
+      const color = new THREE.Color(this.game.player2.options.color);
+      const style = `rgba(${color.r * 255 | 0}, ${color.g * 255 | 0}, ${color.b * 255 | 0}, 0.5`;
+      this.ctx.strokeStyle = style;
+      this.ctx.stroke();
+      if(-i + 5 + this.game.scores[1] > 0) {
+        this.ctx.fillStyle = `rgba(${2 * color.r * 255 | 0}, ${2 * color.g * 255 | 0}, ${2 * color.b * 255 | 0}, 0.8)`;
+        this.ctx.fill();
+      }
+    }
+  }
+
   this.texture.needsUpdate = true;
 };
 
